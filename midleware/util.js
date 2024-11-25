359cc3b2d853fb67b0674e3d10a2c81e324deacc
Ramdani <ramdanipurnama539@gmail.com>
2024-11-25 21:15:16 +0700

    fixing router

---
+++ midleware/util.js
@@ -0,0 +1,10 @@
+// import {ObjectId} from 'mongodb'
+
+// export const validateId = (id) => {
+//     const db = 
+//     const 
+
+// }
+
+// module.exports = {
+//     },
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,33 @@
+const { name } = require('ejs');
 var express = require('express');
 var router = express.Router();
 const path = require('path');
 
-// module.exports = function (db) {
-  /* GET users . */
-  router.get('/', function(req, res, next) {
+  router.get('/', async function (req, res) {
+    const { page = 1, limit = 5, query = '', sortBy = 'id', sortMode = 'desc' } = req.query;
     
-    res.render('list');
+    try {
+      const offset = (page - 1) * limit;
+      const search ={
+        $or: [
+        {name: new RegExp(query, 'i')}, {phone: new RegExp(query, 'i' )}],
+      }
+
+     
+      res.status(200).json({
+        title: 'MongoDB BREADS(Browse, Read, Edit, Add, Delete, Sorting)',
+        offset,
+        search,
+        limit,
+        query,
+        sortBy,
+        sortMode
+      });
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      res.status(500).json('Internal Server Error');
+    }
   });
 
 module.exports = router;
-
-
 
