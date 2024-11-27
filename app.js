f739d72cb92fa7909bc300385802bbb8b1639adc
Ramdani <ramdanipurnama539@gmail.com>
2024-11-27 16:58:15 +0700

    home page view Done, API change to vanilla and Some routes had bugs

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
-const usersRouter = require('./routes/users');
-const todosRouter = require('./routes/todos');
+const connDb = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/tododb');
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+const usersRouter = require('./routes/users')(connDb);
+// const todosRouter = require('./routes/todos')(connDb); 
 
 const app = express();
 
@@ -19,12 +29,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ROUTES
 app.get('/', (req, res) => {
-  res.render('list');
+  res.render('listUsers');
 });
 
-app.use('/', usersRouter);
-app.use('/todos', todosRouter);
+// API
+app.use('/users', usersRouter);
+// app.use('/todos', todosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,71 +1,77 @@
 var express = require('express');
-const { read } = require('fs');
 var router = express.Router();
-const path = require('path');
 
+module.exports = (connDb) => {
   router.get('/', async function (req, res) {
-    const { page = 1, limit = 5, query = '', sortBy = 'id', sortMode = 'desc' } = req.query;
-    console.log(page, limit, query, sortBy, sortMode);
-    
-    
     try {
-      const offset = (page - 1) * (parseInt(limit) || 5);
-      const search = query ? {
-        $or: [
-          {name: new RegExp(query, 'i')}, {phone: new RegExp(query, 'i' )}],
-      } : {};
-      
-      const data = await db.collection('users').find(search).sort({ [sortBy]: sortMode === 'asc' ? 1 : -1 }).skip(offset).limit(parseInt(limit)).toArray();
-      res.render('list', { data });
+      const page = parseInt(page) || 1;
+      const limit = parseInt(limit) || 5;
+      const offset = (page - 1) * limit;
+      const sortBy = req.query.sortBy || 'id';
+      const sortMode = req.query.sortMode || 'asc';
+      const query = req.query.search || '';
+
+      const total = await connDb.collection('users').countDocuments({ name: { $regex: query, $options: 'i' } });
+      const pages = Math.ceil(total / limit);
+      const rows = await connDb.collection('users')
+        .find({ name: { $regex: query, $options: 'i' } })
+        .sort({ [sortBy]: sortMode === 'asc' ? 1 : -1 })
+        .skip(offset)
+        .limit(limit)
+        .toArray();
+
+      res.status(200).json({
+        data: rows[0],
+        total,
+        pages,
+        page,
+        limit,
+        offset
+      })
     } catch (error) {
       console.error('Error fetching users:', error);
       res.status(500).json('Internal Server Error');
     }
   });
+     
  
-  router.post('/:id', async function (req, res) {
-    try {
-      const { name, phone } = req.body;
-      const response = await fetch('http://localhost:3000/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, phone })
-      });
-      const result = await response.json();
-      console.log('ini data add:',name, result);
-      readData();
-    } catch (error) {
-      console.error('Error creating user:', error);
-      res.status(500).json('Internal Server Error');
-    }
-  });
+ 
+  // router.post('/:id', async function (req, res) {
+  //   try {
+  //     const { name, phone } = req.body;
+  //     const response = await fetch('http://localhost:3000/users', {
+  //       method: 'POST',
+  //       body: JSON.stringify({ name, phone })
+  //     });
+  //     const result = await response.json();
+  //     console.log('ini data add:',name, result);
+  //     readData();
+  //   } catch (error) {
+  //     console.error('Error creating user:', error);
+  //     res.status(500).json('Internal Server Error');
+  //   }
+  // });
 
 
-  router.put('/:id', async function (req, res) {
-    try {
-      await db.collection('users').updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
-      res.status(200).json('User updated successfully');
-    } catch (error) {
-      console.error('Error updating user:', error);
-      res.status(500).json('Internal Server Error');
-    }
-  });
+  // router.put('/:id', async function (req, res) {
+  //   try {
+  //     await db.collection('users').updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+  //     res.status(200).json('User updated successfully');
+  //   } catch (error) {
+  //     console.error('Error updating user:', error);
+  //     res.status(500).json('Internal Server Error');
+  //   }
+  // });
 
-  router.delete('/:id', async function (req, res) {
-    try {
-      await db.collection('users').deleteOne({ _id: new ObjectId(req.params.id) });
-      res.status(200).json('User deleted successfully');
-    } catch (error) {
-      console.error('Error deleting user:', error);
-      res.status(500).json('Internal Server Error');
-    }
-  });
+  // router.delete('/:id', async function (req, res) {
+  //   try {
+  //     await db.collection('users').deleteOne({ _id: new ObjectId(req.params.id) });
+  //     res.status(200).json('User deleted successfully');
+  //   } catch (error) {
+  //     console.error('Error deleting user:', error);
+  //     res.status(500).json('Internal Server Error');
+  //   }
+  // });
 
-
-  router.get('/', (req, res) => {
-    res.render('list');
-  });
-
-
-
-module.exports = router;
-
+  return router;
+}
